ff815a44015cea29404076ad00d1e5e06bc44a1c
Ravindra Wagh <rwagh@customtravelsolutions.com>
2021-05-08 13:44:48 +0530

    vulnerabilities removed

--- src/index.js
+++ src/index.js
@@ -8,37 +8,39 @@ const db = require('./helpers/Data');
 const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'));
 const resolvers = require('./resolvers');
 
-const validate_schema = async (resolve, parent, args, ctx, info) => {
-  try {
-    let valid = ctx.db.isValid(args);
-    if (valid) {
-      let result = resolve();
-      return result;
-    } else {
-      return Error("Syntax error in schema!");
+const validate_schema = async(resolve, parent, args, ctx, info) => {
+    try {
+        let valid = ctx.db.isValid(args);
+        if (valid) {
+            let result = resolve();
+            return result;
+        } else {
+            return Error("Syntax error in schema!");
+        }
+    } catch (err) {
+        return Error(err);
     }
-  } catch (err) {
-    return Error(err);
-  }
 }
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 const server = new GraphQLServer({
-  schema: schema, context: req => ({
-    ...req,
-    db
-  }), middlewares: [validate_schema]
+    schema: schema,
+    context: req => ({
+        ...req,
+        db
+    }),
+    middlewares: [validate_schema]
 })
 
 const options = {
-  port: process.env.PORT,
-  endpoint: process.env.DAL_ENDPOINT,
-  subscriptions: '/subscriptions',
-  playground: '/playground',
+    port: process.env.PORT,
+    endpoint: "/",
+    subscriptions: '/subscriptions',
+    playground: '/',
 }
 
 server.start(options, ({ port }) => {
-  console.log(
-    `Server started, listening on port ${port}.`,
-  )
+    console.log(
+        `Server started, listening on port ${port}.`,
+    )
 });
